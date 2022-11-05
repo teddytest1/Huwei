882eaff97a1a5ac92940ba80c867ccaf94e9db00
teddytest1 <80005541+teddytest1@users.noreply.github.com>
2022-11-05 18:05:09 +0800

    123

--- Scripts.js
+++ Scripts.js
@@ -4,11 +4,4 @@ $(document).ready(function(){
     $(".modal-body").html("<img src='"+t+"' class='modal-img'>");
     $("#myModal").modal();
   });
-  
-  $("video").click(function(){
-    var v = $("video > source");
-    var t = v.attr("src");
-    $(".modal-body").html("<video class='model-vid' controls><source src='"+t+"' type='video/mp4'></source></video>");
-    $("#myModal").modal();  
-  });
   });//EOF Document.ready
