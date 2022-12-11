ef0a0108049937a10c2b6bb420144062b93775f5
teddytest1 <x0929180007@gmail.com>
2022-12-11 15:20:45 +0800

    123

---
+++ Map.js
@@ -0,0 +1,45 @@
+$(function(){
+
+    $("#btn1").mouseover(function(){
+        $("#P1").css("scale","1.3")
+    })
+    $("#btn1").mouseout(function(){
+        $("#P1").css("scale","1")
+    })
+})
+$(function(){
+
+    $("#btn2").mouseover(function(){
+        $("#P2").css("scale","1.3")
+    })
+    $("#btn2").mouseout(function(){
+        $("#P2").css("scale","1")
+    })
+})
+$(function(){
+
+    $("#btn3").mouseover(function(){
+        $("#P3").css("scale","1.3")
+    })
+    $("#btn3").mouseout(function(){
+        $("#P3").css("scale","1")
+    })
+})
+$(function(){
+
+    $("#btn4").mouseover(function(){
+        $("#P4").css("scale","1.3")
+    })
+    $("#btn4").mouseout(function(){
+        $("#P4").css("scale","1")
+    })
+})
+$(function(){
+
+    $("#btn5").mouseover(function(){
+        $("#P5").css("scale","1.3")
+    })
+    $("#btn5").mouseout(function(){
+        $("#P5").css("scale","1")
+    })
+})
