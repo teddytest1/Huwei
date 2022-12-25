571b40b8ed60cb2347863d38e422807bac046c15
teddytest1 <x0929180007@gmail.com>
2022-12-26 04:54:54 +0800

    126

--- Map.js
+++ Map.js
@@ -43,3 +43,10 @@ $(function(){
         $("#P5").css("scale","1")
     })
 })
+
+var tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+});
