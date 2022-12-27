e6947e2afc718205472fb67eb41fa7206230da05
teddytest1 <x0929180007@gmail.com>
2022-12-27 18:35:45 +0800

    123

---
+++ Test5.js
@@ -0,0 +1,10 @@
+// $(document).load(function() { 
+//     $("#status").delay(5000).fadeOut(3000);
+//     $("#preloader").delay(7000).fadeOut(3000);
+// })
+
+$(document).ready(function(){
+    $(window).load(function(){  //load函數
+        $("#status").hide();
+    });
+});
